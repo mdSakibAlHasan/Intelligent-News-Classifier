9672ed008c524ebf25909485a03faea0141f781b
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-12-03 09:31:10 +0600

    fahim code of staming added

---
+++ IntelligentNewsClassifier/src/steaming/Main.java
@@ -0,0 +1,11 @@
+package steaming;
+public class Main {
+    public static void main(String[] args) {
+        String inputString = "In the quietude of the twilight, a gentle breeze rustled the leaves, carrying with it the fragrant whispers of nature. The moon, a silvery orb in the indigo sky, cast a serene glow upon the landscape. Crickets played a symphony, and stars adorned the heavens, painting a canvas of tranquility.";
+        String inputString2 = "Such an analysis can reveal features that are not easily visible from the variations in the individual genes and can lead to a picture of expression that is more biologically transparent and accessible to interpretation";
+        String inputString3 = "playing plays played working works work play worked";
+        Stemming stemming = new Stemming();
+        stemming.stem(inputString3);
+
+    }
+}
